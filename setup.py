7bc2213f02a4fcd3ca4ad215229268356d333aa8
Austin Weir <austin.weir@gmail.com>
2020-06-08 11:37:40 -0500

    Fix to register adapter

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read().split('\n')[1]
 
 setuptools.setup(
-    name="intake-sql-adapter", # Replace with your own username
+    name="intake-sql-adapter",
     version="0.0.1",
     author="Austin Weir",
     author_email="noreply@noreply.com",
@@ -13,6 +13,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/aweir12/intake-sql-adapter",
     packages=setuptools.find_packages(),
+    package_data={'': ['*.csv', '*.yml', '*.html']},
+    entry_points={
+        'intake.drivers': [
+            'sql_table = intake_sql_adapter.intake_sql_adapter:SQLTable',
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache 2.0 License",
