85989fe46fe5a44ddba066d1c5948a8a099497f7
Austin Weir <austin.weir@gmail.com>
2020-06-08 09:43:40 -0500

    Updates

--- intake_sql_adapter/intake_sql_adapter.py
+++ intake_sql_adapter/intake_sql_adapter.py
@@ -1,9 +1,39 @@
 from intake.source import base
 from . import __version__
 
-class SQLTest():
-    name = 'sql'
-    version = __version__
+class SQLTable(base.DataSource):
+    """
+    Docstring goes here!
 
-    def __init__(self, uri):
-        self.first_name = uri
+    Parameters
+    ----------
+    uri: str or None
+        Full connection string in sqlalchemy syntax
+    sql_table: str
+        Query expression to pass to the DB backend
+    sql_kwargs: dict
+        Further arguments to pass to pandas.read_sql
+    """
+    name = 'sql_table'
+    version = __version__
+    container = 'dataframe'
+    partition_access = True
+
+    def __init__(self, uri, sql_table, sql_kwargs={}, metadata={}):
+        self._init_args = {
+            'uri': uri,
+            'sql_table': sql_table,
+            'sql_kwargs': sql_kwargs,
+            'metadata': metadata,
+        }
+
+        self._uri = uri
+        self._sql_table = sql_table
+        self._sql_kwargs = sql_kwargs
+        self._dataframe = None
+
+        super(SQLTable, self).__init__(metadata=metadata)
+
+
+    def read(self):
+        return "Hello World"
