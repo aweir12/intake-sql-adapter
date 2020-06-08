e9d981117ca520bde7eb0d26e90bf478b268fb1a
Austin Weir <austin.weir@gmail.com>
2020-06-08 09:27:51 -0500

    Fixing install

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/aweir12/intake-sql-adapter",
-    packages=setuptools.find_packages(),
+    packages=['intake_sql_adapter'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache 2.0 License",
